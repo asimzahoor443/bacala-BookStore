adacebdcb0a39059c4c26524e42f0343abdf8633
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-31 13:58:41 +0500

    docs:done with Header.jsx now updating Navbar.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import Navbar from './Navbar';
+import { CgMenuLeft } from 'react-icons/cg';
+import { RiShoppingBag4Line, RiUserLine } from 'react-icons/ri';
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="fixed top-0 w-full left-0 right-0 z-50">
+      <div className="bg-white py-2.5 max-padd-container flexBetween border-b border-slate-900/10 rounded transition-all duration-300">
+        <Link to={'/'} className="flex-1 flex items-center justify-start">
+          <img
+            src={logo}
+            alt=""
+            height={36}
+            width={36}
+            className="hidden sm:flex mr-2"
+          />
+          <h4 className="bold-24">Bacala</h4>
+        </Link>
+        {/* NavBar */}
+        <div className="flex-1 ">
+          <Navbar />
+        </div>
+        {/* Right Side */}
+        <div className="flex-1 flex items-center justify-end gap-x-3 sm:gap-x-10 ">
+          <CgMenuLeft className="text-2xl xl:hidden cursor-pointer" />
+          <Link to={'/cart'} className="flex relative">
+            <RiShoppingBag4Line className="text-[33px] bg-secondary text-primary p-1.5 rounded-full" />
+            <span className="bg-primary ring-1 ring-slate-900/5 medium-14 absolute left-5 -top-2.5 flexCenter w-5 h-5 rounded-full shadow-md"></span>
+          </Link>
+          <div>
+            <div></div>
+            <button className="btn-outline flexCenter gap-x-2">
+              Login <RiUserLine />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
