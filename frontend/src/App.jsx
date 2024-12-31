41b2efd893c24887277d087254bd53e1a3574c5d
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-31 11:57:12 +0500

    docs:created pages and updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="overflow-hidden bg-primary">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
