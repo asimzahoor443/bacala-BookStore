883ce632cf187ab44d2956aa40371613a972b69e
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-01-02 10:28:13 +0500

    feat:update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
-
-const Navbar = () => {
-  return <div>Navbar</div>;
+import { TbHomeFilled } from 'react-icons/tb';
+import { IoLibrary, IoMailOpen } from 'react-icons/io5';
+import { FaRegWindowClose } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+const Navbar = ({ containerStyles, toggleMenu, menuOpened }) => {
+  const navItems = [
+    {
+      to: '/',
+      label: 'Home',
+      icon: <TbHomeFilled />,
+    },
+    {
+      to: '/shop',
+      label: 'Shop',
+      icon: <IoLibrary />,
+    },
+    {
+      to: '/contact',
+      label: 'Contact',
+      icon: <IoMailOpen />,
+    },
+  ];
+  return (
+    <nav className={containerStyles}>
+      {navItems.map(({ to, label, icon }) => (
+        <div key={label} className="inline-flex relative top-1">
+          <NavLink>
+            <span>{icon}</span>
+            <span>{label}</span>
+          </NavLink>
+        </div>
+      ))}
+    </nav>
+  );
 };
 
 export default Navbar;
